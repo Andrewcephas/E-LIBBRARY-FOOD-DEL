b0c7699b8c66398f998de99b278c79de81f32123
andrew cephas <ngumbaucephas@gmail.com>
2025-04-24 07:39:55 +0300

    Update site

--- backed/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://andrew:41513501@cluster0.gz1og.mongodb.net/food-del').then(()=>console.log("DB connected"));
-}
-
